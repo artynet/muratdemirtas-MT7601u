7a1aa108d6996526e12d7a4a3f197c3847342ca5
root <root@runner-c6871076-project-22-concurrent-0>
2021-06-11 14:39:33 +0000

    removing prebuilt objects

--- src/os/linux/.mt7601Uap.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/mt7601u/src/os/linux/mt7601Uap.ko := ld -r  -T ./scripts/module-common.lds --build-id  -o /home/pi/mt7601u/src/os/linux/mt7601Uap.ko /home/pi/mt7601u/src/os/linux/mt7601Uap.o /home/pi/mt7601u/src/os/linux/mt7601Uap.mod.o
--- src/os/linux/.rtutil7601Uap.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/mt7601u/src/os/linux/rtutil7601Uap.ko := ld -r  -T ./scripts/module-common.lds --build-id  -o /home/pi/mt7601u/src/os/linux/rtutil7601Uap.ko /home/pi/mt7601u/src/os/linux/rtutil7601Uap.o /home/pi/mt7601u/src/os/linux/rtutil7601Uap.mod.o
